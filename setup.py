e29c6f6764cf5ed63b6d412999ee791f632f7066
Julien Anguenot <julien@anguenot.org>
2016-08-26 15:33:48 -0500

    Update deps

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    ['kafka-python==1.2.2']
+    ['kafka-python==1.3.1']
 ]
 
 test_requirements = ['bumpversion==0.5.3',
                      'wheel>=0.29.0',
                      'watchdog==0.8.3',
-                     'flake8==2.6.2',
+                     'flake8==3.0.4',
                      'tox==2.3.1',
-                     'coverage==4.0.3',
-                     'Sphinx==1.3.6',
+                     'coverage==4.2',
+                     'Sphinx==1.4.6',
                     ],
 
 setup(
