95e7d670894585df4d7033b032ebb02aa0998a82
Julien Anguenot <julien@anguenot.org>
2017-06-21 13:05:28 -0500

    Use Ubuntu trusty.

--- tests/test_kafkadriver.py
+++ tests/test_kafkadriver.py
@@ -33,7 +33,7 @@ class TestKafkaDestinaton(unittest.TestCase):
         date_str = 'Jun 22 12:49:16'
         ts = date_str_to_timestamp(date_str)
         # FIXME will break next year
-        expected_ts = '1498153756'
+        expected_ts = '1498135756'
         self.assertEqual(expected_ts, ts)
 
     def test_parser_str_list(self):
